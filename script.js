a4f0d080687ca83a5452a6882b40da10ca325ac9
kaiqueRodriguex <kaiquerodriguex@icloud.com>
2025-04-23 20:36:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-let display = document.getElementById("display");
-function adicionarValor(valor) {
-    display.value += valor;
-}
-function calcular() {
-    try {
-        display.value = eval(display.value);
-    } catch {
-        display.value = "Erro";
-    }
-}
-
 
